d5c3f7b7d8523832065a68f681409e407c1ff0a8
ltbd78 <ltbd78@gmail.com>
2023-04-27 18:47:52 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [activeNav, setActiveNav] = useState(false);
+  console.log(activeNav)
   return (
-    <div className='App'>
-      <div className="navbar">navbar</div>
-      <div className="page">
+    <div className={activeNav ? "App activeNav" : "App"}>
+      <div className='nav-bar'>
+        <nav className={activeNav ? "nav-items activeNav" : "nav-items"}>
+          <a>Link 1</a>
+          <a>Link 2</a>
+        </nav>
+        <button className='dropdown' onClick={() => setActiveNav(!activeNav)}></button>
+      </div>
+      <div className='page'>
         page
         <div className='content'></div>
         <div className='content'></div>
